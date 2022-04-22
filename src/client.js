3579afa89b0afc40b1973e15da5e7795c54ff72c
Shadman <sakibshadman51@gmail.com>
2022-04-22 17:10:37 +0600

    last one

--- src/client.js
+++ src/client.js
@@ -17,11 +17,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: "0um9c3r6",
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: "skIzP0dGCy6FtvNqAwcaTqwuOuryjMSSBdXeXkLTavprxiZsjK6ztQhhJBxKuPUzsRp7ogWAhqH4U44ZI7m0zM5IIrP3gOQbngs7yeScl3G0x1h4K53vR7jbJLKD5hXrKN2Z1jeaIBKKeXW3h8ZnfKAzOydSJtbilBYR7CMiHYYvq3xSVAib",
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
